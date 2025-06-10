652687775dd8ebd7c718d43029b6dfadaece7a88
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-10 20:59:39 -0300

    add #39 - Combination Sum

--- 39-combination-sum/README.md
+++ 39-combination-sum/README.md
@@ -3,4 +3,4 @@
 **Dificuldade:** Medium  
 **URL:** https://leetcode.com/problems/combination-sum/
 
-Given an array of distinct integers `candidates` and a target integer `target`, return a list of all unique combinations of `candidates` where the chosen numbers sum to `target`. You may return the combinations in any order. You may use the same number from `candidates` an unlimited number of times.
+Given an array of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target. You may use the same number from candidates an unlimited number of times.
--- 39-combination-sum/solution.js
+++ 39-combination-sum/solution.js
@@ -1,15 +1,18 @@
-def combinationSum(candidates, target):
-    def backtrack(remaining, combination, start):
-        if remaining == 0:
-            result.append(list(combination))
-            return
-        for i in range(start, len(candidates)):
-            if candidates[i] > remaining:
-                continue
-            combination.append(candidates[i])
-            backtrack(remaining - candidates[i], combination, i)
-            combination.pop()
-    
-    result = []
-    backtrack(target, [], 0)
-    return result
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def backtrack(remaining, combo, start):
+            if remaining == 0:
+                res.append(list(combo))
+                return
+            elif remaining < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                combo.append(candidates[i])
+                backtrack(remaining - candidates[i], combo, i)
+                combo.pop()
+        
+        backtrack(target, [], 0)
+        return res
