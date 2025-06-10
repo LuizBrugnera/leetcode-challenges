5eccb0a2c4c03cdf6907ede89d19b5a8efdb1cb9
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-10 20:11:37 -0300

    add #94 - Binary Tree Inorder Traversal

---
+++ 94-binary-tree-inorder-traversal/README.md
@@ -0,0 +1,6 @@
+# Binary Tree Inorder Traversal (LeetCode #94)
+
+**Dificuldade:** Medium  
+**URL:** https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
---
+++ 94-binary-tree-inorder-traversal/solution.js
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorderTraversal(root):
+    result = []
+    def traverse(node):
+        if node:
+            traverse(node.left)
+            result.append(node.val)
+            traverse(node.right)
+    traverse(root)
+    return result
