0729d2333b7bdaa058e33c8ac9f1c3a5c108c8f6
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-11 20:48:37 -0300

    add #199 - Binary Tree Right Side View

---
+++ 199-binary-tree-right-side-view/README.md
@@ -0,0 +1,6 @@
+# Binary Tree Right Side View (LeetCode #199)
+
+**Dificuldade:** Medium  
+**URL:** https://leetcode.com/problems/binary-tree-right-side-view/
+
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
---
+++ 199-binary-tree-right-side-view/solution.js
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def rightSideView(root):
+    if not root:
+        return []
+    result, queue = [], [root]
+    while queue:
+        result.append(queue[-1].val)
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return result
