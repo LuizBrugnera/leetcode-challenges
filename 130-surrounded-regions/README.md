5afa10e7b25c2db4fe779369432ea0ea38a6b69b
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-10 21:47:38 -0300

    add #130 - Surrounded Regions

---
+++ 130-surrounded-regions/README.md
@@ -0,0 +1,6 @@
+# Surrounded Regions (LeetCode #130)
+
+**Dificuldade:** Medium  
+**URL:** https://leetcode.com/problems/surrounded-regions/
+
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'. A region is captured by flipping all 'O's into 'X's in that surrounded region.
---
+++ 130-surrounded-regions/solution.js
@@ -0,0 +1,27 @@
+def solve(board):
+    if not board:
+        return
+    
+    rows, cols = len(board), len(board[0])
+    
+    def dfs(r, c):
+        if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
+            return
+        board[r][c] = 'E'
+        dfs(r - 1, c)
+        dfs(r + 1, c)
+        dfs(r, c - 1)
+        dfs(r, c + 1)
+    
+    for r in range(rows):
+        for c in range(cols):
+            if r in [0, rows - 1] or c in [0, cols - 1]:
+                if board[r][c] == 'O':
+                    dfs(r, c)
+    
+    for r in range(rows):
+        for c in range(cols):
+            if board[r][c] == 'O':
+                board[r][c] = 'X'
+            elif board[r][c] == 'E':
+                board[r][c] = 'O'
