9dc77fb269293c2dca0e399e14391d2a739a70b4
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-11 18:44:59 -0300

    add #15 - 3Sum

--- 15-3sum/solution.js
+++ 15-3sum/solution.js
@@ -1,22 +1,23 @@
-def threeSum(nums):
-    nums.sort()
-    res = []
-    for i in range(len(nums) - 2):
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
-        l, r = i + 1, len(nums) - 1
-        while l < r:
-            total = nums[i] + nums[l] + nums[r]
-            if total < 0:
-                l += 1
-            elif total > 0:
-                r -= 1
-            else:
-                res.append([nums[i], nums[l], nums[r]])
-                while l < r and nums[l] == nums[l + 1]:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
                     l += 1
-                while l < r and nums[r] == nums[r - 1]:
+                elif s > 0:
                     r -= 1
-                l += 1
-                r -= 1
-    return res
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
