0a7e56b06c8336f2ffd4a898f587a0236af143c0
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-12 03:12:35 -0300

    add #78 - Subsets

--- 78-subsets/solution.js
+++ 78-subsets/solution.js
@@ -1,8 +1,8 @@
 def subsets(nums):
-    result = []
+    res = []
     def backtrack(start, path):
-        result.append(path)
+        res.append(path)
         for i in range(start, len(nums)):
             backtrack(i + 1, path + [nums[i]])
     backtrack(0, [])
-    return result
+    return res
