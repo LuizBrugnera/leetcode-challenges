203a3c5f6044d1a8fdd694f10acda272f9c10934
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-12 00:48:35 -0300

    add #150 - Evaluate Reverse Polish Notation

---
+++ 150-evaluate-reverse-polish-notation/README.md
@@ -0,0 +1,6 @@
+# Evaluate Reverse Polish Notation (LeetCode #150)
+
+**Dificuldade:** Medium  
+**URL:** https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
---
+++ 150-evaluate-reverse-polish-notation/solution.js
@@ -0,0 +1,16 @@
+def evalRPN(tokens):
+    stack = []
+    for token in tokens:
+        if token in "+-*/":
+            b, a = stack.pop(), stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(int(a / b))  
+        else:
+            stack.append(int(token))
+    return stack[0]
