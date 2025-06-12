1f57c2ab63b88064b513a9206682b19011bd224e
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-12 00:24:36 -0300

    add #64 - Minimum Path Sum

--- 64-minimum-path-sum/README.md
+++ 64-minimum-path-sum/README.md
@@ -3,4 +3,4 @@
 **Dificuldade:** Medium  
 **URL:** https://leetcode.com/problems/minimum-path-sum/
 
-Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of the numbers along its path. You can only move down or right.
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of the numbers along its path. Note that you can only move either down or right at any point in time.
--- 64-minimum-path-sum/solution.js
+++ 64-minimum-path-sum/solution.js
@@ -1,12 +1,10 @@
 def minPathSum(grid):
-    if not grid or not grid[0]:
-        return 0
     m, n = len(grid), len(grid[0])
     for i in range(m):
         for j in range(n):
             if i == 0 and j == 0:
                 continue
-            top = grid[i - 1][j] if i > 0 else float('inf')
-            left = grid[i][j - 1] if j > 0 else float('inf')
+            top = grid[i-1][j] if i > 0 else float('inf')
+            left = grid[i][j-1] if j > 0 else float('inf')
             grid[i][j] += min(top, left)
     return grid[-1][-1]
